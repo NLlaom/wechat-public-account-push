b2be5d283a5d0658e5796d1ee4118299b7738d4f
想赢马 <105422177+NLlaom@users.noreply.github.com>
2024-06-26 16:32:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,19 +13,50 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '09c2e656707a530a38e793467b9694c2',
 
-  PROVINCE: '广东',
-  CITY: '深圳',
-
   USERS: [
     {
       // 想要发送的人的名字
       name: '小唐',
+      PROVINCE: '广东',
+      CITY: '深圳',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oauYr6wPFqUxCNB4oiF_FMqIGFDQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '-Ww2hEAT7YWBuFbJb4A78P6b5AzO4Giwpb0gCG9cG6A',
+      useTemplateId: 'Dabt-d64wBUocAoYxstCLbCaBzfyOD9s8cblKUa7FxQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-08',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '笨蛋', year: '2002', date: '02-22',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2022', date: '06-09',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-09-08' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2022-09-09' },
+      ],
+    },
+    {
+      // 想要发送的人的名字
+      name: '小马',
+      PROVINCE: '福建',
+      CITY: '龙岩',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oauYr6wPFqUxCNB4oiF_FMqIGFDQ',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'Dabt-d64wBUocAoYxstCLbCaBzfyOD9s8cblKUa7FxQ',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '02-22',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -46,7 +77,7 @@ const USER_CONFIG = {
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
-    },
+    }
   ],
 
 
