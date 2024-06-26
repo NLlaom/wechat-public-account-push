8e207c5c3b4173102c5d35d4225a8a065e3bcf2b
想赢马 <105422177+NLlaom@users.noreply.github.com>
2024-06-26 14:36:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'wx05b14b56a5a02a8a',
+  CALLBACK_TEMPLATE_ID: 'CPiFsrwAiw7hq0v6KhDz54jeixSV2R9t7LRuH7shDAw',
 
   CALLBACK_USERS: [
     {
