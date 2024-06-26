8c52e67c90069e05bd98b709761c9ae004db4e04
想赢马 <105422177+NLlaom@users.noreply.github.com>
2024-06-26 16:12:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oauYr6wPFqUxCNB4oiF_FMqIGFDQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Cpap4CRvPoWMU6lj6LdNZmTKLI7dhrN5HQYGL8PtVtc',
+      useTemplateId: '2ftliZgVTxp6acBPzaXwbefM9Qwbst2GcAMIk_Prymk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-08',
       festivals: [
